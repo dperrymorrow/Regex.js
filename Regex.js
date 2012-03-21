361dad1c9e437cbbef2599b4a5f296cdd698035c
David Morrow <dperrymorrow@gmail.com>
2012-03-21 09:18:36 -0600

    added several more regex functions

--- Regex.js
+++ Regex.js
@@ -1,4 +1,3 @@
-
 if(window.dpm == undefined){
   window.dpm ={};
 }
@@ -7,4 +6,36 @@ dpm.Regex = {}
 // adds commas to
 dpm.Regex.digits = function(number){
   return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+}
+
+dpm.Regex.removeComments = function(string){
+  return string.toString().replace(/\/\*.+?\*\/|\/\/.*(?=[\n\r])/, "$1");
+}
+
+dpm.Regex.tabsToSpaces = function(string){
+  return string.toString().replace( /s{\t}{ }gm/, "$1");
+}
+
+dpm.Regex.tabsToSpaces = function(string){
+  return string.toString().replace( /s{\t}{ }gm/, "$1");
+}
+
+dpm.Regex.singleSpaceOnly = function(string){
+  return string.toString().replace( /s{ +}{ }gm/, "$1");
+}
+
+dpm.Regex.singleSpaceOnly = function(string){
+  return string.toString().replace( /s{ +}{ }gm/, "$1");
+}
+
+dpm.Regex.removeLeadingWhitespace = function(string){
+  return string.toString().replace( /s{^\s+}{}gm/, "$1");
+}
+
+dpm.Regex.removeTrailingWhitespace = function(string){
+  return string.toString().replace( /s{\s+$}{}gm/, "$1");
+}
+
+dpm.Regex.removeLineBreaks = function(string){
+  return string.toString().replace( /s{\n}{}gm/, "$1");
 }
