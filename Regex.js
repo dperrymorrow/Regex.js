5d2620b267d58630282c82a8279224c2763a8d4d
David Morrow <dperrymorrow@gmail.com>
2012-04-17 12:21:45 -0600

    added uuid and camelCaseToWords

--- Regex.js
+++ Regex.js
@@ -35,3 +35,17 @@ dpm.Regex.removeLineBreaks = function(string){
 dpm.Regex.htmlEntities = function(string){
   return String(string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
+
+dpm.Regex.camelCaseToWords = function(str){
+  return str.replace(/([A-Z])/g, " $1");
+}
+
+dpm.Regex.uuid = function(){
+  var s = [], itoh = '0123456789ABCDEF';
+  for (var i = 0; i <36; i++) s[i] = Math.floor(Math.random()*0x10);
+  s[14] = 4;  // Set 4 high bits of time_high field to version
+  s[19] = (s[19] & 0x3) | 0x8;  // Specify 2 high bits of clock sequence
+  for (var i = 0; i <36; i++) s[i] = itoh[s[i]];
+  s[8] = s[13] = s[18] = s[23] = '-';
+  return s.join('');
+}
